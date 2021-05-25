5dd1be70f66c86335f252a3e4ee13e6ae0d75cd2
DmytroTahiltsev <75026973+DmytroTahiltsev@users.noreply.github.com>
2021-05-25 10:32:30 +0300

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-console.log(1)
-document.querySelector('.header__burger').addEventListener('click', (event)=>{
-    event.currentTarget.classList.toggle('active')
-    document.querySelector('.header__menu').classList.toggle('active')
-    document.body.classList.toggle('lock')
-})
